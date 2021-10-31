040ab1aa79395bcb3d92da5b3a6817d913a8693b
Leonnardo Nascimento <leonnardoenriquesn@gmail.com>
2021-10-31 14:16:30 -0700

    initial commit

--- server.py
+++ server.py
@@ -9,6 +9,16 @@ def hello_world():
 def datas(num):
     return render_template('datas.html',num = num)
 
+@app.route('/lists')
+def render_lists():
+    # Soon enough, we'll get data from a database, but for now, we're hard coding data
+    users = [
+        {'first_name' : 'Michael', 'last_name' : 'Choi'},
+        {'first_name' : 'John', 'last_name' : 'Supsupin'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+    return render_template("lists.html", random_numbers = [3,1,5], users = users)
 
 if __name__=="__main__":
     app.run(debug=True)
