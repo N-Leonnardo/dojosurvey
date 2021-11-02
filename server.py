273c1e61baf572a8143cac9cda08618a26214874
Leonnardo Nascimento <leonnardoenriquesn@gmail.com>
2021-11-01 22:06:53 -0700

    initial commit

--- server.py
+++ server.py
@@ -1,24 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__)
 
+app = Flask(__name__)
+app.secret_key = 'keep it secret, keep it safe' # set a secret key for security purposes
+
 @app.route('/')
-def hello_world():
-    return render_template('index.html')
+def index():
+    return render_template("index.html")
 
-@app.route('/datas/<int:num>/')
-def datas(num):
-    return render_template('datas.html',num = num)
+@app.route('/users', methods=['POST'])
+def create_user():
+    print("Got Post Info")
+    print(request.form)
+    session['username'] = request.form['name']
+    session['userlocation'] = request.form['location']
+    session['userlanguage'] = request.form['language']
+    session['usermessage'] = request.form['message']
+    return redirect('/show')
 
-@app.route('/lists')
-def render_lists():
-    # Soon enough, we'll get data from a database, but for now, we're hard coding data
-    users = [
-        {'first_name' : 'Michael', 'last_name' : 'Choi'},
-        {'first_name' : 'John', 'last_name' : 'Supsupin'},
-        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
-        {'first_name' : 'KB', 'last_name' : 'Tonel'}
-    ]
-    return render_template("lists.html", random_numbers = [3,1,5], users = users)
+@app.route('/show')
+def show_user():
+    return render_template('show.html', name_on_template=session['username'], email_on_template=session['useremail'])
+
 
 if __name__=="__main__":
     app.run(debug=True)
